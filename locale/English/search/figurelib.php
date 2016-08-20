6ccd739b444d1280738ce23f0405584124f89982
Catzenjaeger <catzenjaeger@gmail.com>
2016-08-20 15:26:46 +0200

    Add files via upload

---
+++ locale/English/search/figurelib.php
@@ -0,0 +1,11 @@
+<?php
+$locale['f400'] = "Figures";
+$locale['f401'] = "Figure/s found matching search criteria: ";
+$locale['f402'] = "0 Figure found matching search criteria";
+$locale['f403'] = "[NEW]";
+$locale['f404'] = "Submitter:";
+$locale['f405'] = "Manufacturer:";
+$locale['f406'] = "Publish date:";
+$locale['f407'] = "Added on:";
+$locale['f408'] = "Views:";
+?>
