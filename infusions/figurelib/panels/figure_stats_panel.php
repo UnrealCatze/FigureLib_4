1b56bc80bee065c4218d51019b1894a6f90c4ea4
Catzenjaeger <catzenjaeger@gmail.com>
2016-08-21 20:30:52 +0200

    Update figure_stats_panel.php

--- infusions/figurelib/panels/figure_stats_panel.php
+++ infusions/figurelib/panels/figure_stats_panel.php
@@ -36,54 +36,91 @@ $fil_settings = get_settings("figurelib");
 opentable("<strong>Figure Stats</strong>");
 
 
-//////////////////////////////////
-// gesamtzahl aller Figuren
-$allFiguresCounter = dbcount("(figure_id)",             DB_FIGURE_ITEMS,       "figure_freigabe='1'");	
-		
-		echo "<span>Gesamtzahl aller Figuren: </span><span class='badge'>".$allFiguresCounter."</span>\n";
-		echo "<br>";
-//////////////////////////////////	
+// GESAMTZAHL ALLER FIGUREN //////////////////////////////////
+				$allFiguresCounter = dbcount("(figure_id)", DB_FIGURE_ITEMS, "figure_freigabe='1'");
+				
+		echo "<div class='table-responsive'>\n";
+		echo "<div class='row'>\n";	
+				
+					echo "<div class='col-lg-4 col-md-4 col-sm-4 col-xs-4'>\n";
+					echo "<div class='navbar-default'>";	
+					echo "<span>Gesamtzahl aller Figuren: </span>\n";
+					echo "</div></div>\n";
 
-
-
+					echo "<div class='col-lg-4 col-md-4 col-sm-4 col-xs-4'>\n";
+					echo "<span class='badge'>".$allFiguresCounter."</span>\n";
+					echo "</div>\n";
 	
+		echo "</div>\n";
+		echo "</div>\n";
+
+		// Gesamtzahl Figuren in Userbesitz //////////////////////////////////
 	
-//////////////////////////////////
-// Teuerste Figur
+				$allFiguresCounterUser = dbcount("(figure_userfigures_id)",  DB_FIGURE_USERFIGURES, "");	
 
-		$resultteuerste = dbquery("
-					SELECT 
-						tb.figure_id, tb.figure_submitter, tb.figure_retailprice, tb.figure_usedprice, tb.figure_freigabe, tb.figure_pubdate, tb.figure_scale, tb.figure_title, tb.figure_manufacturer, tb.figure_brand, tb.figure_datestamp, tb.figure_cat, 
-						tbc.figure_cat_id, tbc.figure_cat_name, 
-						tbu.user_id, tbu.user_name, tbu.user_status, tbu.user_avatar, 
-						tbm.figure_manufacturer_name, 
-						tbb.figure_brand_name, 
-						tby.figure_year_id, tby.figure_year, 
-						tbs.figure_scale_id, tbs.figure_scale_name, 							
-						fuf.figure_userfigures_figure_id, fuf.figure_userfigures_user_id 
-					FROM ".DB_FIGURE_ITEMS." AS tb
-					LEFT JOIN ".DB_USERS." AS tbu ON tb.figure_submitter=tbu.user_id
-					LEFT JOIN ".DB_FIGURE_USERFIGURES." AS fuf ON fuf.figure_userfigures_figure_id=tb.figure_id
-					LEFT JOIN ".DB_FIGURE_CATS." AS tbc ON tb.figure_cat=tbc.figure_cat_id
-					LEFT JOIN ".DB_FIGURE_MANUFACTURERS." AS tbm ON tbm.figure_manufacturer_id = tb.figure_manufacturer
-					LEFT JOIN ".DB_FIGURE_BRANDS." AS tbb ON tbb.figure_brand_id = tb.figure_brand
-					LEFT JOIN ".DB_FIGURE_SCALES." AS tbs ON tbs.figure_scale_id = tb.figure_scale
-					LEFT JOIN ".DB_FIGURE_YEARS." AS tby ON tby.figure_year_id = tb.figure_pubdate
-					WHERE ".(multilang_table("FI") ? "tb.figure_language='".LANGUAGE."' AND" : "")." tb.figure_freigabe='1' 
-					ORDER BY tb.figure_retailprice DESC LIMIT 0,1 
+		echo "<div class='table-responsive'>\n";
+		echo "<div class='row'>\n";	
+				
+					echo "<div class='col-lg-4 col-md-4 col-sm-4 col-xs-4'>\n";
+					echo "<div class='navbar-default'>";	
+					echo "<span>Gesamtzahl Figuren in Userbesitz:</span>\n";
+					echo "</div></div>\n";
 
-				");
+					echo "<div class='col-lg-4 col-md-4 col-sm-4 col-xs-4'>\n";
+					echo "<span class='badge'>".$allFiguresCounterUser."</span>\n";
+					echo "</div>\n";
+	
+		echo "</div>\n";
+		echo "</div>\n";
+
+// TEUERSTE FIGUR //////////////////////////////////
+
+						$resultteuerste = dbquery("
+							SELECT 
+								tb.figure_id, tb.figure_submitter, tb.figure_retailprice, tb.figure_usedprice, tb.figure_freigabe, tb.figure_pubdate, tb.figure_scale, tb.figure_title, tb.figure_manufacturer, tb.figure_brand, tb.figure_datestamp, tb.figure_cat, 
+								tbc.figure_cat_id, tbc.figure_cat_name, 
+								tbu.user_id, tbu.user_name, tbu.user_status, tbu.user_avatar, 
+								tbm.figure_manufacturer_name, 
+								tbb.figure_brand_name, 
+								tby.figure_year_id, tby.figure_year, 
+								tbs.figure_scale_id, tbs.figure_scale_name, 							
+								fuf.figure_userfigures_figure_id, fuf.figure_userfigures_user_id 
+							FROM ".DB_FIGURE_ITEMS." AS tb
+							LEFT JOIN ".DB_USERS." AS tbu ON tb.figure_submitter=tbu.user_id
+							LEFT JOIN ".DB_FIGURE_USERFIGURES." AS fuf ON fuf.figure_userfigures_figure_id=tb.figure_id
+							LEFT JOIN ".DB_FIGURE_CATS." AS tbc ON tb.figure_cat=tbc.figure_cat_id
+							LEFT JOIN ".DB_FIGURE_MANUFACTURERS." AS tbm ON tbm.figure_manufacturer_id = tb.figure_manufacturer
+							LEFT JOIN ".DB_FIGURE_BRANDS." AS tbb ON tbb.figure_brand_id = tb.figure_brand
+							LEFT JOIN ".DB_FIGURE_SCALES." AS tbs ON tbs.figure_scale_id = tb.figure_scale
+							LEFT JOIN ".DB_FIGURE_YEARS." AS tby ON tby.figure_year_id = tb.figure_pubdate
+							WHERE ".(multilang_table("FI") ? "tb.figure_language='".LANGUAGE."' AND" : "")." tb.figure_freigabe='1' 
+							ORDER BY tb.figure_retailprice DESC LIMIT 0,1 
+
+						");
 				
 					if (dbrows($resultteuerste) != 0) {
 						while ($data = dbarray($resultteuerste)) {
-						
-						
-						echo "<span>H&ouml;chster Neupreis:</span> ".$data['figure_retailprice']." (".$data['figure_title']." / ".$data['figure_manufacturer_name'].")";
+																		
+					echo "<div class='table-responsive'>\n";
+					echo "<div class='row'>\n";	
+				
+						echo "<div class='col-lg-4 col-md-4 col-sm-4 col-xs-4'>\n";
+						echo "<div class='navbar-default'>";	
+						echo "<span>H&ouml;chster Neupreis:</span>\n";
+						echo "</div></div>\n";
+
+						echo "<div class='col-lg-4 col-md-4 col-sm-4 col-xs-4'>\n";
+						echo "<span class=''>".$data['figure_retailprice']." (".$data['figure_title']." / ".$data['figure_manufacturer_name'].")</span>\n";
+						echo "</div>\n";
+	
+				echo "</div>\n";
+				echo "</div>\n";
+												
 					}
 						}
 						
-//////////////////////////////////
-// Höchster Gebrauchtpreis
+
+// HÖCHSTER GEBRAUCHTPREIS //////////////////////////////////
 
 		$resultteuerstegebraucht = dbquery("
 					SELECT 
@@ -109,24 +146,26 @@ $allFiguresCounter = dbcount("(figure_id)",             DB_FIGURE_ITEMS,       "
 				");
 				
 					if (dbrows($resultteuerstegebraucht) != 0) {
-						while ($data = dbarray($resultteuerstegebraucht)) {
-						
-							echo "<br>";
-							echo "<span>H&ouml;chster Gebrauchtpreis:</span> ".$data['figure_usedprice']." (".$data['figure_title']." / ".$data['figure_manufacturer_name'].")";		
+						while ($data = dbarray($resultteuerstegebraucht)) {	
+
+					echo "<div class='table-responsive'>\n";
+					echo "<div class='row'>\n";	
+				
+						echo "<div class='col-lg-4 col-md-4 col-sm-4 col-xs-4'>\n";
+						echo "<div class='navbar-default'>";	
+						echo "<span>H&ouml;chster Gebrauchtpreis:</span>\n";
+						echo "</div></div>\n";
+
+						echo "<div class='col-lg-4 col-md-4 col-sm-4 col-xs-4'>\n";
+						echo "<span class=''>".$data['figure_usedprice']." (".$data['figure_title']." / ".$data['figure_manufacturer_name'].")</span>\n";
+					
+					echo "</div>\n";
+					echo "</div>\n";
+					echo "</div>\n";						
 					}
 						}
 //////////////////////////////////
 
-
-//////////////////////////////////
-// Gesamtzahl Figuren in Userbesitz
-	
-$allFiguresCounterUser = dbcount("(figure_userfigures_id)",             DB_FIGURE_USERFIGURES,       "");	
-									
-						echo "<br>";
-						echo "<span>Gesamtzahl Figuren in Userbesitz:</span> <span class='badge'>".$allFiguresCounterUser."</span>\n";
-					
-//////////////////////////////////
 
 
 // Close Site
